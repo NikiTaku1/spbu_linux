2a10ac9bef89a63b1f431220355b7af51d60fec6
Yuriko <117597772+NikiTaku1@users.noreply.github.com>
2024-11-28 00:04:00 +0300

    Add files via upload

--- linux_debt/main.py
+++ linux_debt/main.py
@@ -4,7 +4,7 @@ from tkinter import ttk
 import threading
 import subprocess
 
-class Monitor:
+class MonitorT:
 
     #################
     ### Interface ###
@@ -12,7 +12,7 @@ class Monitor:
     def __init__(self, root):
         self.root = root
         self.root.title("Traffic Monitor")
-        self.root.geometry("950x350")
+        self.root.geometry("950x700")
 
         self.ip_count = {}
         self.sus_ips = set()
@@ -28,13 +28,15 @@ class Monitor:
         frame_block = tk.Frame(root)
         frame_block.grid(row=0, column=2, padx=5, pady=5, sticky="n")
 
+        style = ttk.Style()
+        style.configure("Treeview", rowheight = 20)
 
         tk.Label(frame_all, text="Received").pack(side="top")
-        self.table_all = ttk.Treeview(frame_all, columns=("IP", "Port", "Size"), show="headings", height=10)
+        self.table_all = ttk.Treeview(frame_all, columns=("IPs", "Ports", "Packets"), show="headings", height=30)
 
-        self.table_all.heading("IPs", text="IP Address")
+        self.table_all.heading("IPs", text="IP")
         self.table_all.heading("Ports", text="Port")
-        self.table_all.heading("Packets", text="Size")
+        self.table_all.heading("Packets", text="Packet Size")
         self.table_all.column("IPs", width = 150, stretch = tk.NO)
         self.table_all.column("Ports", width = 150, stretch = tk.NO)
         self.table_all.column("Packets", width = 150, stretch = tk.NO)
@@ -46,17 +48,17 @@ class Monitor:
         button_frame.pack(side="top", fill="x")
 
         self.start_button = tk.Button(button_frame, text="Start", command=self.begin_mon)
-        self.start_button.pack(side="left", fill="x", expand = True, padx=5)
+        self.start_button.pack(side="left", fill="x", expand = True, padx=5, pady=5)
 
         self.stop_button = tk.Button(button_frame, text="Stop", command=self.stop_mon)
-        self.stop_button.pack(side="right", fill="x", expand = True, padx=5)
+        self.stop_button.pack(side="right", fill="x", expand = True, padx=5, pady=5)
 
 
-        tk.Label(frame_sus, text="Flagged IPs").pack(side="top")
-        self.table_sus = ttk.Treeview(frame_sus, columns=("IP", "Reason"), show="headings", height=10)
+        tk.Label(frame_sus, text="Flagged").pack(side="top")
+        self.table_sus = ttk.Treeview(frame_sus, columns=("IPs", "Reason"), show="headings", height=30)
 
-        self.table_sus.heading("IPs", text="IP Address")
-        self.table_sus.heading("Reason", text="Reason")
+        self.table_sus.heading("IPs", text="IP")
+        self.table_sus.heading("Reason", text="Flag")
         self.table_sus.column("IPs", width = 150, stretch = tk.NO)
         self.table_sus.column("Reason", width = 150, stretch = tk.NO)
 
@@ -68,10 +70,10 @@ class Monitor:
         self.block_button.pack(fill="x", padx=0, pady=5)
 
 
-        tk.Label(frame_block, text="Blocked IPs").pack(side="top")
-        self.table_block = ttk.Treeview(frame_block, columns=("IP",), show="headings", height=10)
+        tk.Label(frame_block, text="Blocked").pack(side="top")
+        self.table_block = ttk.Treeview(frame_block, columns=("IPs",), show="headings", height=30)
 
-        self.table_block.heading("IPs", text="IP Address")
+        self.table_block.heading("IPs", text="IP")
         self.table_block.column("IPs", width = 150, stretch = tk.NO)
 
         self.table_block.pack(side="top", fill="both", expand=True)
@@ -106,7 +108,7 @@ class Monitor:
         self.start_button.config(state="disabled")
         self.stop_button.config(state="normal")
 
-        monitoring_thread = threading.Thread(target=self.start_mon)
+        monitoring_thread = threading.Thread(target=self.mon)
         monitoring_thread.daemon = True
         monitoring_thread.start()
         print("Started.")
@@ -166,5 +168,5 @@ class Monitor:
 
 if __name__ == "__main__":
     root = tk.Tk()
-    monitor = Monitor(root)
+    monitor = MonitorT(root)
     root.mainloop()
